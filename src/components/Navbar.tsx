03cccaa35f040fe053fe3648b4918d197c08db4c
TallamGilbert <tallamgilbert02@gmail.com>
2026-05-22 14:59:44 +0300

    feat: add navbar structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,31 @@
+import Link from 'next/link';
+
+const navLinks = [
+  { label: 'About', href: '#about' },
+  { label: 'Program', href: '#program' },
+  { label: 'Curriculum', href: '#curriculum' },
+  { label: 'Pricing', href: '#pricing' },
+  { label: 'FAQ', href: '#faq' },
+];
+
 export default function Navbar() {
   return (
     <nav>
-      <p>Navbar</p>
+      <div>
+        <div>
+          <Link href="/">Reduzer School</Link>
+
+          <div>
+            {navLinks.map(({ label, href }) => (
+              <Link key={label} href={href}>
+                {label}
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <Link href="#apply">Apply Now</Link>
+      </div>
     </nav>
   );
 }
