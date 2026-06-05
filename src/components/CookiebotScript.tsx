49da3bd8bf755625c51d6b51acacd8b1050f93e9
Dorah <momanyidora@gmail.com>
2026-06-05 20:36:29 +0300

    Feature/UI polish (#88)
    removed Cookiebot consent integration with PostHog analytics in the
    CookiebotScript.tsx file
    
    ---------
    
    Co-authored-by: Gilbert Tallam <155532936+TallamGilbert@users.noreply.github.com>

--- src/components/CookiebotScript.tsx
+++ src/components/CookiebotScript.tsx
@@ -1,37 +1,20 @@
 'use client';
+
 import Script from 'next/script';
-import posthog from 'posthog-js';
-import { useEffect } from 'react';
 
 export default function CookiebotScript() {
-  useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      opt_out_capturing_by_default: true,
-    });
-    const handleAccept = () => {
-      posthog.opt_in_capturing();
-      posthog.capture('cookie_consent_accepted');
-    };
+  const cookiebotId = process.env.NEXT_PUBLIC_COOKIEBOT_ID;
 
-    const handleDecline = () => {
-      posthog.opt_out_capturing();
-      posthog.capture('cookie_consent_declined');
-    };
-    window.addEventListener('CookiebotOnAccept', handleAccept);
-    window.addEventListener('CookiebotOnDecline', handleDecline);
-
-    return () => {
-      window.removeEventListener('CookiebotOnAccept', handleAccept);
-      window.removeEventListener('CookiebotOnDecline', handleDecline);
-    };
-  }, []);
+  if (!cookiebotId) {
+    console.warn('Cookiebot ID is missing');
+    return null;
+  }
 
   return (
     <Script
       id="Cookiebot"
       src="https://consent.cookiebot.com/uc.js"
-      data-cbid="1d803704-ef37-4292-92d7-3280f6bffa19"
+      data-cbid={cookiebotId}
       data-blockingmode="auto"
       data-cfasync="false"
     />
