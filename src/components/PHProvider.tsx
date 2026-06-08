0995ddc17fc6d82924e7783e751f3f88eb6e9202
Vincent <ookovincent616@gmail.com>
2026-06-08 09:35:57 +0300

    set the defaults to '2026-01-30'

--- src/components/PHProvider.tsx
+++ src/components/PHProvider.tsx
@@ -47,6 +47,7 @@ function initPostHog() {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host:
       process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://psthgeu.reduzer.tech',
+    defaults: '2026-01-30',
 
     capture_pageview: false,
     capture_utmparams: true,
