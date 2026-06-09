da5e3f634c61d07b66ff8eb5ac0a2e8c9696e701
Vincent <ookovincent616@gmail.com>
2026-06-09 12:48:38 +0300

    revert the wrangler.file naming

--- src/components/PHProvider.tsx
+++ src/components/PHProvider.tsx
@@ -33,12 +33,12 @@ let posthogInitialized = false;
 function initPostHog() {
   if (typeof window === 'undefined') return;
   if (posthogInitialized) return;
-  if (!process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN) {
+  if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
     console.warn('[PostHog] Missing API key');
     return;
   }
 
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN, {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host:
       process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://psthgeu.reduzer.tech',
     defaults: '2026-01-30',
