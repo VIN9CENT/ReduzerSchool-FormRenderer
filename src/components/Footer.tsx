fe99e35709750d07e035bc8022fedca31cb486e8
amosotieno-254 <amosotieno429@gmail.com>
2026-06-09 12:00:53 +0300

    added admission link (#97)
    added admission link tot he footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -121,16 +121,13 @@ export default function Footer() {
             <div className="w-28 border-b border-red-500 mt-4 mb-8"></div>
             <ul className="space-y-8 text-sm text-gray-200">
               <li>
-                <Link href="https://reduzer-school.reduzer.tech/#apply">
-                  Admission
-                </Link>
+                <Link href="https://school.reduzer.tech/#apply">Admission</Link>
               </li>
             </ul>
           </div>
         </div>
 
         {/* BUTTONS */}
-
         <a
           href="https://wa.me/+254769267965"
           target="_blank"
